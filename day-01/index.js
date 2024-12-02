ea83ffcbbbe6ac6b37b645f17d7bb27ac65aaaab
Flemming Eriksson <fleeri91@gmail.com>
2024-12-02 15:35:52 +0100

    part 2

--- day-01/index.js
+++ day-01/index.js
@@ -31,7 +31,7 @@ const parseInput = (rawInput) => {
  * @param parsedInput
  * @returns
  */
-const solution = (parsedInput) => {
+const solutionPart1 = (parsedInput) => {
   const leftSide = parsedInput.map((item) =>
     parseInt(item.split(/\s+/)[0], 10)
   );
@@ -51,12 +51,35 @@ const solution = (parsedInput) => {
   return totalDifference;
 };
 
+const solutionPart2 = (parsedInput) => {
+  const leftSide = parsedInput.map((item) =>
+    parseInt(item.split(/\s+/)[0], 10)
+  );
+  const rightSide = parsedInput.map((item) =>
+    parseInt(item.split(/\s+/)[1], 10)
+  );
+
+  let totalValue = 0;
+
+  for (let i = 0; i < leftSide.length; i++) {
+    const value = leftSide[i];
+    const count = rightSide.filter((num) => num === value).length;
+
+    if (count > 0) {
+      totalValue += value * count;
+    }
+  }
+
+  return totalValue;
+};
+
 const main = () => {
   const day = path.basename(__dirname).split("-")[1];
   const rawInput = readInput(day);
   const parsedInput = parseInput(rawInput);
 
-  console.log(`Day ${day}:`, solution(parsedInput));
+  console.log(`Day ${day}: part 1`, solutionPart1(parsedInput));
+  console.log(`Day ${day}: part 2`, solutionPart2(parsedInput));
 };
 
 main();
